4d93d5efe6b7316959cb96a2d7872f44d21da0f7
Tyler Beason <tbeas12@gmail.com>
2020-04-30 12:46:27 -0700

    lots more polish and test suite

--- src/FinancialPortfolios.jl
+++ src/FinancialPortfolios.jl
@@ -8,12 +8,7 @@ module FinancialPortfolios
 
 export FinancialPortfolio
 export portfolioreturn, update!
-export names, weights
-
-
-
-
-
+export weights
 
 
 
@@ -29,6 +24,7 @@ export names, weights
 A (possibly named) collection of portfolio weights.
 Weights can be an dictionary-like object matching asset identifiers with weights or just a vector of weights.
 
+_**Example**_
 ```
 w = [0.5, 0.25, 0.25]
 FP_vec = FinancialPortfolio(w)
@@ -43,16 +39,13 @@ FP_fancydict = FinancialPortfolio(fancy_dict)
 ```
 """
 struct FinancialPortfolio{T}
-    positions::T 
+    positions::T
+    FinancialPortfolio{T}(x) where {T} = new(x)
 end
-# T should be <:AbstractDict or AbstractDictionary or Vector
+# T should be <:AbstractDict or Dictionaries.AbstractDictionary or AbstractVector
+# NamedTuple works except is immutable so breaks update!
+FinancialPortfolio(x::T) where {T} = FinancialPortfolio{T}(copyandnormalize(x))
 
-"""
-`names(fp::FinancialPortfolio)`
-
-Recover the names or identifiers of the assets in the portfolio.
-"""
-names(fp::FinancialPortfolio) = collect(keys(fp.positions))
 
 
 
@@ -70,10 +63,15 @@ weights(fp::FinancialPortfolio) = fp.positions
 """
 `portfolioreturn(fp::FinancialPortfolio,ret)`
 
-Compute the portfolio return. If the portfolio has named weights, the returns should as well.
+Compute the weighted portfolio return. If the portfolio weights have names or identifiers, the returns should as well.
+The portfolio is allowed to contain only a subset of the supplied asset returns, but not the other way around.
 
+_**Example**_
 ```
-
+w = [0.5, 0.25, 0.25]
+r = [0.1, 0.1, -0.1]
+FP = FinancialPortfolio(w)
+portfolioreturn(FP,r)
 ```
 """
 function portfolioreturn(fp::FinancialPortfolio,ret)
@@ -90,6 +88,17 @@ end
 `update!(fp::FinancialPortfolio,ret)`
 
 Updates the weights of the portfolio in place. Returns the period portfolio return.
+
+The portfolio is allowed to contain only a subset of the supplied asset returns, but not the other way around.
+
+_**Example**_
+```
+w = [0.5, 0.25, 0.25]
+r = [0.1, 0.1, -0.1]
+FP = FinancialPortfolio(w)
+update!(FP,r)
+FP
+```
 """
 function update!(fp::FinancialPortfolio,ret)
     portreturn = portfolioreturn(fp,ret)
@@ -102,7 +111,12 @@ function update!(fp::FinancialPortfolio,ret)
 end
 
 
+##################### UNEXPORTED AND/OR EXTENDED BASE METHODS
+"""
+`FinancialPortfolios.normalize!(fp::FinancialPortfolio)`
 
+Normalize portfolio weights to sum to 1. Used during [`update!`](@ref).
+"""
 function normalize!(fp::FinancialPortfolio)
     fac = sum(values(fp.positions))
     for k in eachindex(fp.positions)
@@ -111,10 +125,68 @@ function normalize!(fp::FinancialPortfolio)
     return nothing
 end
 
-# add custom Base.show for pretty printing
-# should add inner constructor to normalize weights upon construction
-# need a method to check names match
-# add copy, similar, ==, ...?
-# forward things like size, length, ...?
+"""
+`FinancialPortfolios.copyandnormalize(x)`
+
+Normalize portfolio weights to sum to 1. Used during construction.
+"""
+function copyandnormalize(x0)
+    x = copy(x0)
+    fac = sum(values(x))
+    for k in eachindex(x)
+        x[k] = x[k] / fac
+    end
+    return x
+end
+
+
+"""
+`keys(fp::FinancialPortfolio)`
+
+Recover the names or identifiers of the assets in the portfolio.
+"""
+Base.keys(fp::FinancialPortfolio) = collect(keys(fp.positions))
+
+
+
+Base.length(fp::FinancialPortfolio) = length(fp.positions)
+
+Base.copy(fp::FinancialPortfolio) = FinancialPortfolio(copy(fp.positions))
+
+function Base.similar(fp::FinancialPortfolio)
+    ew = 1 / length(fp.positions)
+    # initialize an equal weight portfolio with same keys
+    fp2 = copy(fp)
+    for k in eachindex(fp2.positions)
+        fp2.positions[k] = ew
+    end
+    return fp2
+end
+
+function Base.:(==)(fp1::FinancialPortfolio{T1},fp2::FinancialPortfolio{T2}) where {T1,T2}
+    # short circuit if these are the exact same object
+    fp1.positions === fp2.positions && return true
+
+    # otherwise check for equality of types, keys, values
+    typematch = T1 == T2
+    !typematch && return false
+
+    namematch = sort(keys(fp1)) == sort(keys(fp2))
+    !namematch && return false
+
+    for k in eachindex(fp1.positions)
+        vm_i = fp1.positions[k] == fp2.positions[k]
+        if !vm_i
+            return false
+        end
+    end
+    
+    return true
+end
+
+
+
+
+
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,139 @@
-using FinancialPortfolios
+using FinancialPortfolios, LinearAlgebra, Dictionaries
 using Test
+
+
+
+w = [0.5, 0.25, 0.25]
+nm = ["a", "b", "c"]
+plain_dict = Dict(Iterators.zip(nm,w))
+plain_dict_sym = Dict(Iterators.zip(Symbol.(nm),w))
+plain_dict_int = Dict(Iterators.zip(1:3,w))
+
+r = [0.1, 0.1, -0.1]
+const trueret1 = dot(w,r)
+const new_weight = w .* (1 .+ r) ./ (1 + trueret1)
+const trueret2 = dot(new_weight,r)
+const ewret = sum(r) / 3
+
+wgt_dict = Dict(Iterators.zip(nm,new_weight))
+wgt_dict_sym = Dict(Iterators.zip(Symbol.(nm),new_weight))
+wgt_dict_int = Dict(Iterators.zip(1:3,new_weight))
+
+ret_dict = Dict(Iterators.zip(nm,r))
+ret_dict_sym = Dict(Iterators.zip(Symbol.(nm),r))
+ret_dict_int = Dict(Iterators.zip(1:3,r))
+
+
+
+
+@testset "Vectors" begin
+    FP = FinancialPortfolio(w)
+    @test portfolioreturn(FP,r) ≈ trueret1
+    update!(FP,r)
+    @test portfolioreturn(FP,r) ≈ trueret2
+    @test length(FP) == length(w)
+    @test FP == copy(FP)
+    FP2 = similar(FP)
+    @test portfolioreturn(FP2,r) ≈ ewret
+    @test sort(keys(FP)) == [1,2,3]
+end
+
+@testset "Dict-String" begin
+    ret = ret_dict
+    wgts = wgt_dict
+    FP = FinancialPortfolio(plain_dict)
+    @test portfolioreturn(FP,ret) ≈ trueret1
+    update!(FP,ret)
+    @test portfolioreturn(FP,ret) ≈ trueret2
+    @test length(FP) == length(w)
+    @test FP == copy(FP)
+    FP2 = similar(FP)
+    @test portfolioreturn(FP2,ret) ≈ ewret
+    @test sort(keys(FP)) == nm
+end
+
+@testset "Dict-Symbol" begin
+    ret = ret_dict_sym
+    wgts = wgt_dict_sym
+    FP = FinancialPortfolio(plain_dict_sym)
+    @test portfolioreturn(FP,ret) ≈ trueret1
+    update!(FP,ret)
+    @test portfolioreturn(FP,ret) ≈ trueret2
+    @test length(FP) == length(w)
+    @test FP == copy(FP)
+    FP2 = similar(FP)
+    @test portfolioreturn(FP2,ret) ≈ ewret
+    @test sort(keys(FP)) == Symbol.(nm)
+end
+
+@testset "Dict-Int" begin
+    ret = ret_dict_int
+    wgts = wgt_dict_int
+    FP = FinancialPortfolio(plain_dict_int)
+    @test portfolioreturn(FP,ret) ≈ trueret1
+    update!(FP,ret)
+    @test portfolioreturn(FP,ret) ≈ trueret2
+    @test length(FP) == length(w)
+    @test FP == copy(FP)
+    FP2 = similar(FP)
+    @test portfolioreturn(FP2,ret) ≈ ewret
+    @test sort(keys(FP)) == [1,2,3]
+end
+
+@testset "HashDictionary-String" begin
+    dict = dictionary(plain_dict)
+    ret = dictionary(ret_dict)
+    wgts = dictionary(wgt_dict)
+    FP = FinancialPortfolio(dict)
+    @test portfolioreturn(FP,ret) ≈ trueret1
+    update!(FP,ret)
+    @test portfolioreturn(FP,ret) ≈ trueret2
+    @test length(FP) == length(w)
+    @test FP == copy(FP)
+    FP2 = similar(FP)
+    @test portfolioreturn(FP2,ret) ≈ ewret
+    @test sort(keys(FP)) == nm
+end
+
+
+@testset "HashDictionary-Symbol" begin
+    dict = dictionary(plain_dict_sym)
+    ret = dictionary(ret_dict_sym)
+    wgts = dictionary(wgt_dict_sym)
+    FP = FinancialPortfolio(dict)
+    @test portfolioreturn(FP,ret) ≈ trueret1
+    update!(FP,ret)
+    @test portfolioreturn(FP,ret) ≈ trueret2
+    @test length(FP) == length(w)
+    @test FP == copy(FP)
+    FP2 = similar(FP)
+    @test portfolioreturn(FP2,ret) ≈ ewret
+    @test sort(keys(FP)) == Symbol.(nm)
+end
+
+
+@testset "HashDictionary-Int" begin
+    dict = dictionary(plain_dict_int)
+    ret = dictionary(ret_dict_int)
+    wgts = dictionary(wgt_dict_int)
+    FP = FinancialPortfolio(dict)
+    @test portfolioreturn(FP,ret) ≈ trueret1
+    update!(FP,ret)
+    @test portfolioreturn(FP,ret) ≈ trueret2
+    @test length(FP) == length(w)
+    @test FP == copy(FP)
+    FP2 = similar(FP)
+    @test portfolioreturn(FP2,ret) ≈ ewret
+    @test sort(keys(FP)) == [1,2,3]
+end
+
+
+@testset "HashDictionary-Symbol-NamedTupleReturn" begin
+    dict = dictionary(plain_dict_sym)
+    ret = (;Iterators.zip(Symbol.(nm),r)...)
+    wgts = dictionary(wgt_dict_sym)
+    FP = FinancialPortfolio(dict)
+    @test portfolioreturn(FP,ret) ≈ trueret1
+    update!(FP,ret)
+    @test portfolioreturn(FP,ret) ≈ trueret2
+end
